139894fb9a9ecb84b96ee917ddbaebb23c4d25b3
Aidan Hibbard <aidanchibbard@gmail.com>
2025-12-24 18:51:51 -0800

    add theme toggle

--- app/components/navigation/header/AppHeader.vue
+++ app/components/navigation/header/AppHeader.vue
@@ -2,12 +2,18 @@
 import {
   SidebarTrigger,
 } from '@/components/shadcn/ui/sidebar'
+
+import ModeToggle from '@/components/ModeToggle.vue'
 </script>
 
 <template>
   <header class="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-    <div class="flex items-center gap-2 px-4">
-      <SidebarTrigger class="-ml-1" />
+    <div class="flex w-full items-center justify-between px-4">
+      <div class="flex items-center gap-2">
+        <SidebarTrigger class="-ml-1" />
+      </div>
+
+      <ModeToggle />
     </div>
   </header>
 </template>
